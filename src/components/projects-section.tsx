c660e08959321c8da1b151a79d4cfd1326d88949
Jakub Gaik <jakub.gaik@gmail.com>
2025-08-16 14:57:37 +0900

    update projects

--- src/components/projects-section.tsx
+++ src/components/projects-section.tsx
@@ -2,11 +2,8 @@ import { useTranslation } from "react-i18next";
 import { PAGE_ORIGIN } from "../constants";
 import { Link, List } from "@yamori-design/react-components";
 
-const PROJECTS_LIST_MAP: Record<
-  string,
-  Array<Record<"name" | "link" | "descriptionKey", string>>
-> = {
-  individual: [
+const PROJECTS_LIST: Array<Record<"name" | "link" | "descriptionKey", string>> =
+  [
     {
       name: "Yamori Design",
       link: PAGE_ORIGIN + "/yamori-design/",
@@ -32,15 +29,7 @@ const PROJECTS_LIST_MAP: Record<
       link: PAGE_ORIGIN + "/daily-goals-tracker/",
       descriptionKey: "dailyGoalsTracker",
     },
-  ],
-  contributing: [
-    {
-      name: "PapuApp",
-      link: "https://papu.app",
-      descriptionKey: "papuApp",
-    },
-  ],
-};
+  ];
 
 export const ProjectsSection: React.FC = () => {
   const { t } = useTranslation(undefined, { keyPrefix: "projectsSection" });
@@ -48,26 +37,20 @@ export const ProjectsSection: React.FC = () => {
   return (
     <section id="projects" className="project-section">
       <h4>{t("title")}</h4>
-      <List>
-        {Object.entries(PROJECTS_LIST_MAP).map(([categoryKey, projects]) => (
-          <List.Item key={categoryKey} label={t(categoryKey)} labelTag="h6">
-            <List bulleted>
-              {projects.map(({ name, link, descriptionKey }) => (
-                <List.Item
-                  key={name}
-                  label={
-                    <Link
-                      href={link}
-                      target={link.startsWith(PAGE_ORIGIN) ? "" : "_blank"}
-                    >
-                      {name}
-                    </Link>
-                  }
-                >
-                  <p>{t(`projectDescription.${descriptionKey}`)}</p>
-                </List.Item>
-              ))}
-            </List>
+      <List bulleted>
+        {PROJECTS_LIST.map(({ name, link, descriptionKey }) => (
+          <List.Item
+            key={name}
+            label={
+              <Link
+                href={link}
+                target={link.startsWith(PAGE_ORIGIN) ? "" : "_blank"}
+              >
+                {name}
+              </Link>
+            }
+          >
+            <p>{t(`projectDescription.${descriptionKey}`)}</p>
           </List.Item>
         ))}
       </List>
